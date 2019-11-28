5cc7bad21bb32b2e73ac1a72f358424a07b6fd11
hanquocadrian <hanquocadrian@gmail.com>
2019-11-28 21:39:18 +0700

    Add De thi mau OOP

---
+++ OOP_DeMau_CK/TranQuocHung_ThiMau/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TranQuocHung_ThiMau
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void phongToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PhongBanForm frmpb = new PhongBanForm();
+            frmpb.MdiParent = this;
+            frmpb.Show();
+        }
+
+        private void nhânViênToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NhanVienForm frmnv = new NhanVienForm();
+            frmnv.MdiParent = this;
+            frmnv.Show();
+        }
+    }
+}
