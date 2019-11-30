40799a32f49987e86186475e141607f5a6e1bb39
hanquocadrian <hanquocadrian@gmail.com>
2019-11-30 14:44:06 +0700

    Add type: pass data from form1 to form2 non Open vs Save

---
+++ DeThiMauOOPNonOS/TranQuocHung_ThiMau/PhongBanForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections;
+
+namespace TranQuocHung_ThiMau
+{
+    public partial class PhongBanForm : Form
+    {
+        #region Attributes
+        private CdmPhongBan xl = new CdmPhongBan();
+        public static Hashtable dspb;
+
+        internal CdmPhongBan Xl { get => xl; set => xl = value; }
+        #endregion
+        #region Method
+        public void hienThi()
+        {
+            lvwPB.Items.Clear();
+            foreach (DictionaryEntry item in Xl.DmPhongBan)
+            {
+                PhongBan p = (PhongBan)item.Value;
+                ListViewItem li = lvwPB.Items.Add(p.MaPhong1);
+                li.SubItems.Add(p.TenPhong1);
+            }
+        }
+        public void hienThiPB(string ma)
+        {
+            PhongBan a = Xl.tim(ma);
+            txtMP.Text = a.MaPhong1;
+            txtTP.Text = a.TenPhong1;
+        }
+        #endregion
+        public PhongBanForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            PhongBan p = new PhongBan();
+            p.MaPhong1 = txtMP.Text;
+            p.TenPhong1 = txtTP.Text;
+
+            if (Xl.them(p))
+            {
+                hienThi();
+                dspb = Xl.DmPhongBan;
+            }
+            else
+                MessageBox.Show("Trùng mã phòng", "Error");
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            foreach (int item in lvwPB.SelectedIndices)
+            {
+                if (Xl.xoa(lvwPB.Items[item].Text))
+                {
+                    hienThi();
+                    dspb = Xl.DmPhongBan;
+                }
+                break;
+            }
+        }
+
+        private void lvwPB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (int item in lvwPB.SelectedIndices)
+            {
+                hienThiPB(lvwPB.Items[item].Text);
+                break;
+            }
+        }
+    }
+}
---
+++ DeThiMauOOPNonOS/TranQuocHung_ThiMau/ThongTin.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace TranQuocHung_ThiMau
+{
+    partial class ThongTin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MSSV: DH51700402";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ho Ten: Trần Quốc Hùng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Lop: D17-TH01";
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(112, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ThongTin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 244);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "ThongTin";
+            this.Text = "ThongTin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
